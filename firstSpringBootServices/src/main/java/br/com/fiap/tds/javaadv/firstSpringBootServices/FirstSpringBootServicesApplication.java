ac486e90331dcee65c64f8e31765730cb58de763
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-19 09:27:36 -0300

    criando o primeiro projeto utilizando spring boot para a materia de java advanced

---
+++ firstSpringBootServices/src/main/java/br/com/fiap/tds/javaadv/firstSpringBootServices/FirstSpringBootServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tds.javaadv.firstSpringBootServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootServicesApplication.class, args);
+	}
+
+}
---
+++ firstSpringBootServices/src/test/java/br/com/fiap/tds/javaadv/firstSpringBootServices/FirstSpringBootServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tds.javaadv.firstSpringBootServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
