80499f19ffbb7577d4d6868746eae98227658f6d
devpedrosena1 <devpedrosena1@gmail.com>
2025-09-02 15:40:10 -0300

    tirando @repository de profileRepository

--- Library/src/main/java/br/com/fiap/tds/javaadv/Library/domainmodel/repositories/ProfileRepository.java
+++ Library/src/main/java/br/com/fiap/tds/javaadv/Library/domainmodel/repositories/ProfileRepository.java
@@ -2,6 +2,7 @@ package br.com.fiap.tds.javaadv.Library.domainmodel.repositories;
 
 import br.com.fiap.tds.javaadv.Library.domainmodel.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
