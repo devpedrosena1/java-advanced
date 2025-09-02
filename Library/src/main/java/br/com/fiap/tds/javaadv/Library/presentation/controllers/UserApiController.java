60b36c6a0acaf44609b637f80e7484ffc76b4fab
devpedrosena1 <devpedrosena1@gmail.com>
2025-09-02 09:51:45 -0300

    adicionando metodos put e patchMapping que faltava no controller de user

--- Library/src/main/java/br/com/fiap/tds/javaadv/Library/presentation/controllers/UserApiController.java
+++ Library/src/main/java/br/com/fiap/tds/javaadv/Library/presentation/controllers/UserApiController.java
@@ -3,6 +3,7 @@ package br.com.fiap.tds.javaadv.Library.presentation.controllers;
 import br.com.fiap.tds.javaadv.Library.domainmodel.User;
 import br.com.fiap.tds.javaadv.Library.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
@@ -48,7 +50,32 @@ public class UserApiController {
 
     }
 
-    // falta um metodo partialUpdate
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable("id") UUID id, @RequestBody User user){
+        if( !this.userService.existsById(id) )
+            return ResponseEntity.notFound().build();
+//        User userFromDatabase = this.userService.findById(id);
+        user.setId(id);
+        return new ResponseEntity<>(
+                this.userService.create(user),
+                HttpStatus.CREATED
+        );
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> partialUpdate(@PathVariable("id") UUID id, @RequestBody User user) {
+        User updatedUser = null;
+        try {
+            updatedUser = this.userService.findById(id);
+            return new ResponseEntity<>(
+                    updatedUser,
+                    HttpStatus.CREATED
+            );
+        } catch (IllegalArgumentException ex) {
+            log.error(ex.getMessage(), ex);
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 
 }
