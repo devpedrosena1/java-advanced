b044ee82027a5d15811e1c409ad90577b380e35b
devpedrosena1 <devpedrosena1@gmail.com>
2025-09-16 09:39:37 -0300

    documentando a API com swagger e api-docs e colocando tratamento de excecoes para possiveis erros nas apis

--- Library/src/main/java/br/com/fiap/tds/javaadv/Library/presentation/controllers/UserApiController.java
+++ Library/src/main/java/br/com/fiap/tds/javaadv/Library/presentation/controllers/UserApiController.java
@@ -3,6 +3,9 @@ package br.com.fiap.tds.javaadv.Library.presentation.controllers;
 import br.com.fiap.tds.javaadv.Library.domainmodel.User;
 import br.com.fiap.tds.javaadv.Library.presentation.transferObjects.UserDTO;
 import br.com.fiap.tds.javaadv.Library.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,15 +24,18 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Tag(name = "Usuarios", description = "Operacões relacionadas a controle de usuários")
 public class UserApiController {
 
     private final UserService userService;
 
+    @Operation(summary = "Listar todos os usuários", method = "GET")
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
         return ResponseEntity.ok(this.userService.findAll());
     }
 
+    @Operation(summary = "Listar todos os usuários", method = "GET")
     @GetMapping("/{id}") //http://localhost:8080/api/users/1
     public ResponseEntity<UserDTO> findById(@PathVariable("id") UUID id){
 //        User user = this.userService.findById(id);
@@ -42,12 +48,14 @@ public class UserApiController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Insere novo usuário", method = "POST")
     @PostMapping
     public ResponseEntity<UserDTO> save( @Valid @RequestBody UserDTO userDto){
         User newUser = this.userService.create(UserDTO.toEntity(userDto));
         return new ResponseEntity<>(UserDTO.fromEntity(newUser), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Remove um usuário pelo ID", method = "DELETE")
     @DeleteMapping
     public ResponseEntity<Void> deleteById(@RequestBody UUID id){
         if( !this.userService.existsById(id ))
@@ -56,6 +64,7 @@ public class UserApiController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Remove um usuário inteiro", method = "DELETE")
     @DeleteMapping("/removeObject")
     public ResponseEntity<Void> delete(@RequestBody UserDTO user){
         if(!this.userService.existsById(user.getId()))
@@ -64,6 +73,7 @@ public class UserApiController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Atualiza um usuário inteiro", method = "PUT")
     @PutMapping("/{id}")
     public ResponseEntity<UserDTO> update(@PathVariable("id") UUID id, @Valid @RequestBody UserDTO userDto){
         if( !this.userService.existsById(id) )
@@ -76,6 +86,7 @@ public class UserApiController {
         );
     }
 
+    @Operation(summary = "Atualiza parcialmente o usuário", method = "PATCH")
     @PatchMapping("/{id}")
     public ResponseEntity<UserDTO> partialUpdate(@PathVariable("id") UUID id, @Valid @RequestBody UserDTO userDto) {
         User updatedUser = null;
@@ -91,12 +102,14 @@ public class UserApiController {
         }
     }
 
+    @Operation(summary = "Busca usuário pelo nome", method = "GET")
     @GetMapping("/name")
     public ResponseEntity<List<UserDTO>> findAllByName(@RequestParam String name){
         return null;
     }
 
     //http://localhost:8080/api/users?email=teste@gmail.com
+    @Operation(summary = "Busca usuário pelo e-mail parametrizado", method = "GET")
     @GetMapping("/")
     public ResponseEntity<List<UserDTO>> findAllByEmail(@RequestParam String email){
 
@@ -109,7 +122,9 @@ public class UserApiController {
 
     }
 
+
     //http://localhost:8080/api/users/paged?page=0&size=10&sort=name,desc
+    @Operation(summary = "Busca todos os usuários paginados", method = "GET")
     @GetMapping("paged")
     public ResponseEntity<Page<UserDTO>> findAllPaged(Pageable pageable){
         return ResponseEntity.ok(
