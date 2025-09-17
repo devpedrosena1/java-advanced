abe3f62d971d90de84c2fecd770bd07e4004de43
devpedrosena1 <devpedrosena1@gmail.com>
2025-09-17 08:18:24 -0300

    criando projeto do checkpoint 2 - Missão Drone com Spring Boot

---
+++ Project-Mission-Drone/src/main/java/br/com/fiap/tds/javaadv/missiondrone/ProjectMissionDroneApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tds.javaadv.missiondrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMissionDroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMissionDroneApplication.class, args);
+	}
+
+}
