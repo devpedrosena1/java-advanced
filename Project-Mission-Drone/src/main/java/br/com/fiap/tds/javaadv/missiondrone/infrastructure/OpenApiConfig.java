e614ae36d69eda32f01e1a6af33307bb81031ff6
devpedrosena1 <devpedrosena1@gmail.com>
2025-09-25 15:51:40 -0300

    adicionando openApiConfig e globalExceptionHandler

--- Project-Mission-Drone/src/main/java/br/com/fiap/tds/javaadv/missiondrone/infrastructure/OpenApiConfig.java
+++ Project-Mission-Drone/src/main/java/br/com/fiap/tds/javaadv/missiondrone/infrastructure/OpenApiConfig.java
@@ -1,7 +1,21 @@
 package br.com.fiap.tds.javaadv.missiondrone.infrastructure;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info( new Info()
+                        .title("Example API")
+                        .version("1.0")
+                        .description("API Checkpoint 2 - Java Advanced.")
+                );
+    }
+
 }
